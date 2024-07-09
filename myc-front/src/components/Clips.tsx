46ba4bbbdbb1e77901138948fcc0958940549eb3
ricepotato <sukjun40@naver.com>
2024-07-09 12:15:04 +0900

    feat: clip component 분리

--- myc-front/src/components/Clips.tsx
+++ myc-front/src/components/Clips.tsx
@@ -13,45 +13,54 @@ export default function Clips({
   return (
     <ul>
       {clips.map((clip) => (
-        <li
-          className="my-2 p-4 flex border min-h-24 rounded-sm w-full justify-between items-center"
-          key={`${clip.id}`}
-        >
-          <div className="flex w-full justify-between items-center ">
-            {clip.type.includes("image") && clip.imageUrl ? (
-              <img src={clip.imageUrl} alt={clip.text} className="h-24" />
-            ) : (
-              <pre>
-                <code>{clip.text}</code>
-              </pre>
-            )}
-            <div className="flex gap-1">
-              {clip.type.includes("text") ? (
-                <FaRegCopy
-                  className="cursor-pointer size-6"
-                  onClick={() => {
-                    navigator.clipboard.writeText(clip.text as string);
-                  }}
-                />
-              ) : (
-                <MdOutlineDownloading
-                  className="cursor-pointer size-6"
-                  onClick={() => {
-                    window.open(clip.imageUrl, "_blank");
-                  }}
-                />
-              )}
-              <MdDeleteOutline
-                className="cursor-pointer size-6"
-                onClick={() => {
-                  deleteClip(clip.id);
-                  onDelete(clip.id);
-                }}
-              />
-            </div>
-          </div>
+        <li key={`${clip.id}`}>
+          <Clip clip={clip} onDelete={onDelete} />
         </li>
       ))}
     </ul>
+  );
+}
+
+function Clip({
+  clip,
+  onDelete,
+}: {
+  clip: IClip;
+  onDelete: (id: string) => void;
+}) {
+  return (
+    <div className="my-2 p-4 min-h-24 border flex w-full justify-between items-center rounded-sm">
+      {clip.type.includes("image") && clip.imageUrl ? (
+        <img src={clip.imageUrl} alt={clip.text} className="h-24" />
+      ) : (
+        <pre>
+          <code>{clip.text}</code>
+        </pre>
+      )}
+      <div className="flex gap-1">
+        {clip.type.includes("text") ? (
+          <FaRegCopy
+            className="cursor-pointer size-6"
+            onClick={() => {
+              navigator.clipboard.writeText(clip.text as string);
+            }}
+          />
+        ) : (
+          <MdOutlineDownloading
+            className="cursor-pointer size-6"
+            onClick={() => {
+              window.open(clip.imageUrl, "_blank");
+            }}
+          />
+        )}
+        <MdDeleteOutline
+          className="cursor-pointer size-6"
+          onClick={() => {
+            deleteClip(clip.id);
+            onDelete(clip.id);
+          }}
+        />
+      </div>
+    </div>
   );
 }
