09fafa2e7c498a19b00f75f36a0dfdafc155ccff
ricepotato <ricepotato40@gmail.com>
2025-01-07 00:51:14 +0900

    feat: add icons, manifest update

--- myc-front/src/index.tsx
+++ myc-front/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./App";
 
@@ -13,6 +14,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
